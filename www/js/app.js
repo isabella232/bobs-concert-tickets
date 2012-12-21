ce2155d7f6988ecb46f8b141e1f851fe41d04bbc
Christopher Groskopf <staringmonkey@gmail.com>
2012-12-21 08:29:52 -0600

    Cleanup coordinates code.

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,21 @@
 $(function() {
     var MIN_ZOOM = 0;
     var MAX_ZOOM = 2;
-    var COORDINATE_MULTIPLIER = 1 / Math.pow(2, MAX_ZOOM - MIN_ZOOM)
+    var COORDINATE_MULTIPLIER = 1 / Math.pow(2, MAX_ZOOM - MIN_ZOOM);
+
+    function xy(x, y) {
+        /*
+         * Convert pixel coords into psuedo-lat-lng.
+         */
+        return new L.LatLng(-y * COORDINATE_MULTIPLIER, x * COORDINATE_MULTIPLIER);
+    }
 
     var superzoom = L.map('superzoom', {
-        center: new L.LatLng(-2531 * COORDINATE_MULTIPLIER, 1196 * COORDINATE_MULTIPLIER),
+        center: xy(1196, 2531),
         zoom: 2,
         minZoom: MIN_ZOOM,
         maxZoom: MAX_ZOOM,
-        maxBounds: new L.LatLngBounds(new L.LatLng(0, 0), new L.LatLng(-7595, 5463)),
+        maxBounds: new L.LatLngBounds(xy(0, 0), xy(5463 - 256, 7595)),
         crs: L.CRS.Simple
     });
 
